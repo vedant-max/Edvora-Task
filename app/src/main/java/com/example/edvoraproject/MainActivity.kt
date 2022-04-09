8b9c03da0f5be62c64c8095cb47ace443c7438b7
Vedant Tiwari <vedanttiw168@gmail.com>
2022-04-09 17:38:26 +0530

    Added fragments

--- app/src/main/java/com/example/edvoraproject/MainActivity.kt
+++ app/src/main/java/com/example/edvoraproject/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.edvoraproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.edvoraproject.databinding.ActivityMainBinding
+import com.example.edvoraproject.ui.NearestRideFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setNearestFragment()
+
+    }
+
+    private fun setNearestFragment(){
+        supportFragmentManager.beginTransaction().replace(binding.mainFrame.id, NearestRideFragment()).commit()
     }
 
 }
